7d88061cc017c0e61b9405c4ad45b60e05872fb8
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-10-09 21:22:26 +0545

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 app.get("/",function(request,response){
     response.send("<h1>Hello brother</h1>");
 })
+app.get("/contact",function(req,res){
+    res.send("Contact me at karanjitrohan@gmail.com")
+})
+
 
 app.listen(3000,function(){
     console.log("Server started in 3000.");
