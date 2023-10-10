647f4403fe33e2ce72dc41fedbab5f4d792b5c56
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-10-10 15:40:45 +0545

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,1 +1,9 @@
 
+const app = express();
+
+// Use body-parser middleware for parsing form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
