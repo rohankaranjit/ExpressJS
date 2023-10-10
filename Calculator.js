64aac1ae0c12d37c0e3ac2181f1fd408de2f444b
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-10-10 15:41:01 +0545

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -7,3 +7,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", function (req, res) {
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
+
+  var result = num1 + num2;
+
+  res.send("The result is " + result);
+});
