301577ce42605eb6ec8b91fd1888c962d0dcd02e
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-10-10 15:42:04 +0545

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,4 +1,21 @@
-
+//Frontend Part
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+</head>
+<body>
+    <form action="/" method="post">
+        <h1>Calculator</h1>
+        <input type="text" placeholder="First Number" name="n1">
+        <input type="text" placeholder="Second Number" name="n2">
+        <button type="submit" value="Calculate">Calculate</button>
+    </form>
+    
+</body>
+</html>
 
 
 
