238be502bcf73a6290c5f22155ad297e43e35067
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2023-10-10 16:12:40 +0545

    Create bmi.js

---
+++ bmi.js
@@ -0,0 +1,24 @@
+const express = require ("express");
+const bodyParser=require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req,res)
+{
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var result = weight/(height*height);
+
+    res.send("The BMI is " + result);
+})
+
+app.listen(3000,function(){
+    console.log("The server is running in 3000");
+})
