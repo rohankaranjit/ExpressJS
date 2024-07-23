fac69669de37d3b508b19a908e3d4dfea7ea8863
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-07-23 22:33:33 +0545

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,3 +3,8 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
