e4617d81f169b8de755d81615fcac92a7e28c9f9
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-07-23 22:33:42 +0545

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -8,3 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
