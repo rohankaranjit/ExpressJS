8b03613fbd30a8bed485b8dde58b5635fd4772d8
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-07-24 14:52:33 +0545

    Update index.js

--- Secret Access Project/index.js
+++ Secret Access Project/index.js
@@ -1,1 +1,13 @@
+import express from "express";
+import bodyParser from "body-parser";
+import{dirname} from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app  = express();
+const port = 3000;
+
+var userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({extended : true}));
