e53600739e1e468e31b9c8e4ec9fe7bbc6e30bb5
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-07-25 22:28:54 +0545

    Update bandNameGenerator.js

--- bandNameGenerator/bandNameGenerator.js
+++ bandNameGenerator/bandNameGenerator.js
@@ -1,1 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
 
+const app = express();
+const port = 3000;
+
+const _dirName =dirname(fileURLToPath(import.meta.url))
+app.use(bodyParser.urlencoded({extended : true}));
+
+var bandName = "";
