338e703d616adaf98b825ddd31ef96e38d041def
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2025-01-05 23:12:14 +0545

    Update custom_middleware.js

--- custom_middleware.js
+++ custom_middleware.js
@@ -1,7 +1,6 @@
 import express from "express";
 const app = express();
 const port = 3000;
-
 function logger(req,res,next) {
   console.log("Request Method is", req.method)
   console.log("Request URL is" , req.url);
